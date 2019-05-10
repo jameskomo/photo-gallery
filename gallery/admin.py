499c9bd481ba9c8056d0fd8af4b7706e1db3bf5a
jameskomo <james.komoh@gmail.com>
2019-05-10 17:07:40 +0300

    Super user account created for Django Admin access, models registered in admin.py and imported as well. Confirmed as imported in localhost:8000/admin

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Image, Location, tags, Category
 
 # Register your models here.
+admin.site.register(Image)
+admin.site.register(Location)
+admin.site.register(Category)
+admin.site.register(tags)
