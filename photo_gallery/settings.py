1e3a221e2573faae520b21f361350eb70c60b3d9
jameskomo <james.komoh@gmail.com>
2019-05-10 15:15:33 +0300

    About navigation and Category drop down button added, view functions redefined and Render used to return views instead of Http response.

--- photo_gallery/settings.py
+++ photo_gallery/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'bootstrap3',
+    'bootstrap4',
     'gallery.apps.GalleryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
