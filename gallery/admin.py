17c4b9145393cda1e99ec60e457a24bd95780b4d
jameskomo <james.komoh@gmail.com>
2019-05-10 17:12:48 +0300

    ModelAdmin class that allows us to customize our models inside our admin page added to admin.py for Location and filter_horizontal property specified that allows ordering of many to many fields and pass in the tags article field

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from .models import Image, Location, tags, Category
 
+class LocationAdmin(admin.ModelAdmin):
+    filter_horizontal =('tags',)
+
 # Register your models here.
 admin.site.register(Image)
-admin.site.register(Location)
+admin.site.register(Location, LocationAdmin)
 admin.site.register(Category)
 admin.site.register(tags)
