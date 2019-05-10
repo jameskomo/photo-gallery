f4cc958502051514025e2241e62c79f72a5e10ed
jameskomo <james.komoh@gmail.com>
2019-05-10 16:55:27 +0300

    Tags class to allow multiple tagging in images. String representation added to models to allow human readable queries. Migrations effected

--- gallery/models.py
+++ gallery/models.py
@@ -5,10 +5,29 @@ class Image(models.Model):
     image_name=models.CharField(max_length=60)
     image_description = models.TextField()
     image_location = models.CharField(default="", max_length = 30) 
-    image_category = models.CharField(default="", max_length = 30) 
+    image_category = models.CharField(default="", max_length = 30)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
 
 class Category(models.Model):
     image_category =  models.ForeignKey(Image, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
+
+class tags(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+
 class Location(models.Model):
     image_location = models.ForeignKey(Image, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(tags)
+
+    def __str__(self):
+        return self.name
+
