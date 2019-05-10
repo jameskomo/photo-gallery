8a258f29039222c6503515e9bd0faef97e2b0cfd
jameskomo <james.komoh@gmail.com>
2019-05-10 16:50:02 +0300

    Models for Image, Location and Category created. Migrations made and relationships set

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image_name=models.CharField(max_length=60)
+    image_description = models.TextField()
+    image_location = models.CharField(default="", max_length = 30) 
+    image_category = models.CharField(default="", max_length = 30) 
+
+class Category(models.Model):
+    image_category =  models.ForeignKey(Image, on_delete=models.CASCADE)
+
+class Location(models.Model):
+    image_location = models.ForeignKey(Image, on_delete=models.CASCADE)
