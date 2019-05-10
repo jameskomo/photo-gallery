1adb4dd38b83f999f2721f77c724bf43314dd035
jameskomo <james.komoh@gmail.com>
2019-05-10 14:10:47 +0300

    Bootstrap installed and added to settings.py, loaded in base template with all necessary CSS and JS blocks. Base route created in app urls.py based on Home view function

--- gallery/urls.py
+++ gallery/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns=[
-    path('', views.home, name='gallery-home')
+    path('', views.home, name='gallery-home'),
+    path('about/', views.about, name='gallery-about'),
 ]
