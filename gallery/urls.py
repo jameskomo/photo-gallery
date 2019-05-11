6ea56a919df1cdffe6f6fa6f73fdc3bcfe4fed22
jameskomo <james.komoh@gmail.com>
2019-05-11 11:28:38 +0300

    Tests created for various methods-delete, save, update and the relevant methods created to pass tests

--- gallery/urls.py
+++ gallery/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-    path('', views.home, name='gallery-home'),
+    path('', views.images, name='gallery-home'),
     path('about/', views.about, name='gallery-about'),
     path('images/', views.images, name='gallery-images'),
     url(r'^search/', views.search_results, name='search_results'),
