2fc1c380e6d50d8c07ff14eba264bf64cca0fe20
jameskomo <james.komoh@gmail.com>
2019-05-11 12:27:46 +0300

    Copy functionality implemented that allows users to copy image links

--- gallery/static/gallery/main.js
+++ gallery/static/gallery/main.js
@@ -1,20 +1,14 @@
-// Get the modal
-var modal = document.getElementById('myModal');
+function myFunction() {
+  /* Get the text field */
+  var copyText = document.getElementById("myInput");
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg');
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
+  /* Select the text field */
+  copyText.select();
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Link Copied: " + copyText.value);
 }
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
-}
+document.Get
